cebc10f4006e8b552aa6da0e3f5283f126fd3e02
ashnadsilva23 <ashnadsilva2309@gmail.com>
2024-07-24 23:10:47 +0530

    signin api completed

--- app.js
+++ app.js
@@ -13,6 +13,39 @@ app.use(cors())
 mongoose.connect("mongodb+srv://ashna:ashna@cluster0.n9qo4.mongodb.net/blogAppDB?retryWrites=true&w=majority&appName=Cluster0")
 
 //signin
+app.post("/signIn",async(req,res)=>{
+    let input =req.body
+    let result=userModel.find({email:req.body.email}).then(
+        (items)=>{
+            if (items.length>0)
+            {
+                const passwordvalidator=bcrypt.compareSync(req.body.password,items[0].password)
+                if (passwordvalidator) {
+                    jwt.sign({email:req.body.email},"blogapp",{expiresIn:"1d"},(error,token)=>{
+                        if (error) {
+                            res.json({"status":"error","errorMessage":error})
+
+                            
+                        } else {
+                            res.json({"status":"success","token":token,"userId":items[0]._id})
+                            
+                        }
+                    })
+                    
+                } else {
+                    res.json({"status":"Incorect password"})
+
+
+                    
+                }
+            }
+            else{
+                res.json({"status":"invalid email id "})
+
+            }
+        }
+    ).catch()
+})
 
 //signup
 app.post("/signUp",async(req,res)=>{
